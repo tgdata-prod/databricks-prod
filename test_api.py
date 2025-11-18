7572b9c6b04ba1006e2898a3371fe22bdda02e4e
tgdata-prod <thomas.george.data@gmail.com>
2025-11-18 17:30:14 +1100

    normalised the api main method to reduce redundancy

--- test_api.py
+++ test_api.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 import os 
 import json 
 import time 
-from datetime import datetime,timedelta
-import sys
+from datetime import datetime
 
 
 def write_api_execution_data(new_api_execution_data: dict ,json_file_path='./api_execution_data.json'):
@@ -30,7 +29,7 @@ def get_university_data_http(http_params: dict, last_execution_data: dict):
     
     load_dotenv()
     COLLEGESCORE_API_KEY =  os.getenv("COLLEGESCORE_API_KEY")
-    all_data = dict()    
+    all_data = []    
 
     last_execution_time = datetime.fromisoformat(last_execution_data['last_execution_time'])
     api_call_count = last_execution_data['api_call_count']
@@ -38,52 +37,32 @@ def get_university_data_http(http_params: dict, last_execution_data: dict):
     
     url = f'https://api.data.gov/ed/collegescorecard/v1/schools?api_key={COLLEGESCORE_API_KEY}'
     
+    page = 0
+    total_pages = 0 
+    while page<=total_pages:
+        http_params.update('')
+        status = requests.get(url, http_params, timeout=60)
 
-    status = requests.get(url, params=http_params)
-    
-    if status and status.status_code==200:
-        try:        
-            if (datetime.now().minute-last_execution_time.minute)<60 and api_call_count<1000:
-                api_call_count += 1 
-            else: 
-                raise Exception('You have reached the api call limit')
-        except requests.HTTPError.strerror as e:
-            print(e)
-                
-    content = json.loads(status.content)
+        if status and status.status_code==200:
+            try:      
+                #Rate Limit (https://collegescorecard.ed.gov/data/api-documentation/)  
+                if (datetime.now()-last_execution_time).min<60 and api_call_count<1000:
+                    api_call_count += 1 
+                else: 
+                    raise Exception('You have reached the api call limit')
+            except requests.HTTPError.strerror as e:
+                print(e)
+                    
+        content = json.loads(status.content)
 
-    metadata = content['metadata']
-    page, total_pages, per_page = metadata['page'], metadata['total'], metadata['perpage']
-
-
-
-    while current_page<total_pages:
-
-        status = requests.get(url, params=http_params)
-
-        response = status.content
-
-        response_pretty = json.loads(response)
-
-        if response_pretty:
-            try:
-                results = response_pretty['results']
-            except json.JSONDecodeError as e:
-                print("object is empty")    
-
+        metadata = content['metadata']
+        page, total_pages = metadata['page'], metadata['total']
         
-    
-        new_metadata = response_pretty['metadata']
-        current_page = new_metadata['current_page']
-        api_call_count+=1
-        all_data.update(results)
+
+        results = content['results'][0]
+
+        all_data.append(results)
+        page = page + 1
         time.sleep(0.5)
-
-    
-    # new_api_execution_data = {'last_execution_time': datetime.now(), \
-    #                           'api_call_count': api_call_count}
-
-
-    
-
-    # return all_data
+        
+    return all_data
