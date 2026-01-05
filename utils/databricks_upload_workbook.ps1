45db86e54ec1883f8736779cd792594e4c7847ad
tgdata-prod <thomas.george.data@gmail.com>
2026-01-05 14:42:26 +1100

    changed output location for databricks upload ps1

--- utils/databricks_upload_workbook.ps1
+++ utils/databricks_upload_workbook.ps1
@@ -1,1 +1,1 @@
-databricks workspace import /Workspace/DatabricksWorkspace/TomsNotebook.ipynb --file C:/Users/lette/projects/Cryptocurrency-Data/databricks/TomsNotebook.ipynb --language PYTHON --overwrite --format
+databricks workspace import /Workspace/DatabricksWorkspace/TomsNotebook.ipynb --file ./databricks/TomsNotebook.ipynb --language PYTHON --overwrite --format
